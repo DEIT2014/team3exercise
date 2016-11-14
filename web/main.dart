09ba2d1cd5a6348375024976aa37c8a7a18ee814
MiniTake <920847228@qq.com>
2016-11-14 16:20:12 +0800

    main函数里面的变化
    加入了几个主要会用到的函数。具体功能后面有解释，大家看了可以的话，不要忘记sync

--- web/main.dart
+++ web/main.dart
@@ -8,3 +8,15 @@ import 'package:polymer/polymer.dart';
 main() async {
   await initPolymer();
 }
+
+void Login()//登录函数
+{}
+void Logup(){}//注册函数
+void Readro(){}///读报函数
+void Random(){}////随机选择数据库中的字符，供用户练习；（应用在三个地方:单个字母、短句、长句）
+void MouseInteraction(){}///通过鼠标与用户进行交互，并且有相关参数记录鼠标的点击时间【页面最设计一个横线加光点横线被分为5段，光点匀速在横线上行动，提醒用户按鼠标…】
+void Judger(){}///判断记录时间的参数的数据与原设定是否大致相似，然后给出判断结果。
+void Recordtime(){}///记录训练开始于结束时间。
+void Level(){}///根据时间判断读报员的等级。
+void Copyro(){}//抄报函数
+void Judgec(){}///判断用户输入的数据与给定数据是否相同，然后给出判断结果。

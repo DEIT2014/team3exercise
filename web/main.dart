8c1fa8598431dec46c6bca01e7354fec0587f6e1
MiniTake <920847228@qq.com>
2016-11-24 16:36:06 +0800

    Revert "实现主要功能"
    This reverts commit cc4685adb8123904984aa75816e5fad6c1da1369.

--- web/main.dart
+++ web/main.dart
@@ -1,30 +1,10 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-import 'dart:html';
-import 'dart:math';
-int time1=0;
-int time2=0;
-int time=0;
 
+import 'package:pg/main_app.dart';
+import 'package:polymer/polymer.dart';
 
-void main() {
-  var a="MouseClick";
-  var s=querySelector("#pid").text=time.toString();
-  querySelector('#sample_text_id2')
-    ..text =a
-    ..onMouseDown.listen(onText1)
-    ..onMouseUp.listen(onText2);
-
+/// [MainApp] used!
+main() async {
+  await initPolymer();
 }
-
-void onText1(MouseEvent event) {
-   time1=event.timeStamp;
-  querySelector('#sample_text_id1').text =time1.toString();
-}
-
-void onText2(MouseEvent event)
-{
-   time2=event.timeStamp;
-  querySelector('#sample_text_id1').text =(time2-time1).toString();
-}
-

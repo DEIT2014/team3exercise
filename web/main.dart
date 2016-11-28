88fbdd4c91fdf67d91570375a28c7746d8c4e181
AilseSmile <1051984732@qq.com>
2016-11-29 01:32:57 +0800

    修改html和server
    修改html和server

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,20 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+HttpRequest request;
+void main(){
+  querySelector("#btnCLICK").onClick.listen(click);
 
-void main() {
-
+}
+void click(MouseEvent e){
+  String url = 'http://localhost:8081/ok/stu';   //?
+  request = new HttpRequest();
+  request.onReadyStateChange.listen(onData);
+  request.open('POST', url);
+  request.send(" your jsonndata");
+}
+void onData(_) {
+  if (request.readyState == HttpRequest.DONE && request.status == 200) {
+    querySelector("#showme").text=request.responseText;
+  }
 }

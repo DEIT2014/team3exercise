7667042ab7534c3907c155c6f31cf23c507d52e8
fireblue204 <459440991@qq.com>
2016-12-13 17:30:37 +0800

    页面变动
    修改了swrite页面，应用了route框架

--- web/main.dart
+++ web/main.dart
@@ -3,9 +3,33 @@
 
 import 'dart:html';
 import 'dart:convert';
+import 'package:route/client.dart';
+import 'package:logging/logging.dart';
+import 'urls.dart' as urls;
 HttpRequest request;
 void main(){
   querySelector("#btnPRLB").onClick.listen(click);
+  //route client
+  new Router()
+    ..addHandler(urls.sign, showSign)//注册界面
+    ..addHandler(urls.main, showMain)//主界面
+    ..addHandler(urls.teach, showTeach)//学习训练界面
+    ..addHandler(urls.learn, showLearn)//教学训练界面
+    ..addHandler(urls.video, showVideo)//视频教学界面
+    ..addHandler(urls.picture, showPicture)//图文教学界面
+    ..addHandler(urls.practice, showPractice)//训练主界面
+    ..addHandler(urls.mread, showMread)//读报主界面
+    ..addHandler(urls.sread, showSread)//单字符读报界面
+    ..addHandler(urls.meread, showMeread)//短语读报界面
+    ..addHandler(urls.mwrite, showMwrite)//抄报主界面
+    ..addHandler(urls.swrite, showSwrite)//单字符抄报界面
+    ..addHandler(urls.mewrite, showMewrite)//短语抄报界面
+    ..addHandler(urls.merror, showMerror)//错题主界面
+    ..addHandler(urls.rerror, showRerror)//读报错题界面
+    ..addHandler(urls.werror, showWerror)//抄报错题界面
+    ..addHandler(urls.end, showEnd)//抄报错题界面
+    ..addHandler(urls.index, (_) => null)//登录界面
+    ..listen();
 
 }
 void click(MouseEvent e) {
@@ -23,4 +47,58 @@ void onData(_) {
     var WordData = questData["Word1"];
     querySelector("#QUES").text =WordData;
   }
+}
+
+//route client
+void showSign(String path){
+
+}
+
+void showMain(String path){
+
+}
+void  showTeach(String path){
+
+}
+void showLearn(String path){
+
+}
+void showVideo(String path){
+
+}
+void showPicture(String path){
+
+}
+void showPractice(String path){
+
+}
+void showMread(String path){
+
+}
+void showSread(String path){
+
+}
+void showMeread(String path){
+
+}
+void showMwrite(String path){
+
+}
+void showSwrite(String path){
+
+}
+void showMewrite(String path){
+
+}
+void showMerror(String path){
+
+}
+void showRerror(String path){
+
+}
+void showWerror(String path){
+
+}
+void showEnd(String path){
+
 }

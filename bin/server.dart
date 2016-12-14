90bbbab5ce12a39353ec72bfcbe25b61f4aa8119
fireblue204 <459440991@qq.com>
2016-12-14 09:51:39 +0800

    route的运用

--- bin/server.dart
+++ bin/server.dart
@@ -2,114 +2,106 @@
  //is governed by a MIT-style license that can be found in the LICENSE file.
 
 
- import 'dart:async';
- import 'dart:io';
- import 'dart:convert';
- import 'package:sqljocky/sqljocky.dart';
- import 'package:args/args.dart';
- import 'package:shelf/shelf.dart' as shelf;
- import 'package:shelf/shelf_io.dart' as io;
- import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
- import 'package:shelf_route/shelf_route.dart';
- import 'dart:math';
+//Copyright (c) 2016, meflyup. All rights reserved. Use of this source code
+//is governed by a MIT-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+import 'package:sqljocky/sqljocky.dart';
+import 'package:args/args.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
+import 'package:shelf_route/shelf_route.dart';
+import 'dart:math';
+
+/*
+*这个案例显示如何综合使用shelf pipeline，middleware和handle；
+*如何接受和响应从客户端发起的post和get请求
+*如何连接数据库及查询数据；
+*如何转换json数据
+*/
 
 
- /*
- *这个案例显示如何综合使用shelf pipeline，middleware和handle；
- *如何接受和响应从客户端发起的post和get请求
- *如何连接数据库及查询数据；
- *如何转换json数据
- */
+Map<String, String> type = new Map();
+Map<String, String> word= new Map();
+Map<String, String> time = new Map();
+final pool = new ConnectionPool(host: "127.0.0.1",
+    port: 3306,
+    user: 'mini',
+//用你自己的账号替代
+    password: 'webex3',
+//用你自己的密码替代
+    db: 'morse_code',
+//用你自己的数据库替代
+    max: 5); //与数据库相连
+
+main(List<String> args) async {
+  //可不关注此处代码
+  var parser = new ArgParser()
+    ..addOption('port', abbr: 'p', defaultsTo: '8080');
+  var result = parser.parse(args);
+  var port = int.parse(result['port'], onError: (val) {
+    //  stdout.writeln('Could not parse port value "$val" into a number.');
+    //  exit(1);
+  });
+//建立路由
+  var myRouter = router();
+  //配置cors参数
+  Map <String, String> corsHeader = new Map();
+  corsHeader["Access-Control-Allow-Origin"] = "*";
+  corsHeader["Access-Control-Allow-Methods"] = 'POST,GET,OPTIONS';
+  corsHeader['Access-Control-Allow-Headers'] =
+  'Origin, X-Requested-With, Content-Type, Accept';
+
+  myRouter.get('/', _echoUserName);
+  myRouter.post('/rest', _echoRequest);
+  //配置cors参数
+  var routerHandler = myRouter.handler;
+  //配置shelf中间件和路由handle
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addMiddleware(
+      shelf_cors.createCorsHeadersMiddleware(corsHeaders: corsHeader))
+      .addHandler(routerHandler);
+
+//启动服务器
+
+  io.serve(handler, '127.0.0.1', port).then((server) {print('Serving at http://${server.address.host}:${server.port}');
+  });
+}
 
 
-
-
- Map<String, String> type = new Map();
- Map<String, String> word= new Map();
- Map<String, String> time = new Map();
- final pool = new ConnectionPool(host: "127.0.0.1",
-     port: 3306,
-     user: 'mini',
- //用你自己的账号替代
-     password: 'webex3',
- //用你自己的密码替代
-     db: 'morse_code',
- //用你自己的数据库替代
-     max: 5); //与数据库相连
-
-
- main(List<String> args) async {
-   //可不关注此处代码
-   var parser = new ArgParser()
-     ..addOption('port', abbr: 'p', defaultsTo: '8080');
-   var result = parser.parse(args);
-   var port = int.parse(result['port'], onError: (val) {
-     //  stdout.writeln('Could not parse port value "$val" into a number.');
-     //  exit(1);
-   });
- //建立路由
-   var myRouter = router();
-   //配置cors参数
-   Map <String, String> corsHeader = new Map();
-   corsHeader["Access-Control-Allow-Origin"] = "*";
-   corsHeader["Access-Control-Allow-Methods"] = 'POST,GET,OPTIONS';
-   corsHeader['Access-Control-Allow-Headers'] =
-   'Origin, X-Requested-With, Content-Type, Accept';
-
-
-   myRouter.get('/', _echoUserName);
-   myRouter.post('/rest', _echoRequest);
-   //配置cors参数
-   var routerHandler = myRouter.handler;
-   //配置shelf中间件和路由handle
-   var handler = const shelf.Pipeline()
-       .addMiddleware(shelf.logRequests())
-       .addMiddleware(
-       shelf_cors.createCorsHeadersMiddleware(corsHeaders: corsHeader))
-       .addHandler(routerHandler);
-
-
- //启动服务器
-
-
-   io.serve(handler, '127.0.0.1', port).then((server) {print('Serving at http://${server.address.host}:${server.port}');
-   });
- }
-
-
-
-
- Future<shelf.Response> _echoUserName(shelf.Request request) async {
-   //从数据库获取数据
-   String t = await getDataFromDb();
-   //把这个post过来的数据有返回给客户端
-   return new shelf.Response.ok(
-       '$t');
- }
-
-
- Future<String> getDataFromDb() async {
-   var results = await pool.query('select * from duizhao');
-   int i = 0;
-   results.forEach((row) {
-     //列出所有用户
-     String index = i.toString();
-     word[index] = row.word;
-     time[index]= row.time;
-     i++;
-   });
- var Randomvar= new Random();
- int n=Randomvar.nextInt(12);
-   String jsonData=word['$n']+" "+ time['$n'];
-  //String jsonData = JSON.encode(word['$n'] +" "+ time['$n']);
-   return jsonData;
- }
-
-
- Future<shelf.Response> _echoRequest(shelf.Request request) async {
-   //接受post过来的数据
-   String content = await request.readAsString();
+Future<shelf.Response> _echoUserName(shelf.Request request) async {
+  //从数据库获取数据
+  String t = await getDataFromDb();
   //把这个post过来的数据有返回给客户端
-   return new shelf.Response.ok(
-       'server susscefullly get the post data from client is: "${content}');
- }
+  return new shelf.Response.ok(
+      '$t');
+}
+
+Future<String> getDataFromDb() async {
+  var results = await pool.query('select * from duizhao');
+  int i = 0;
+  results.forEach((row) {
+    //列出所有用户
+    String index = i.toString();
+    word[index] = row.word;
+    time[index]= row.time;
+    i++;
+  });
+  var Randomvar= new Random();
+  int n=Randomvar.nextInt(12);
+  String jsonData=word['$n']+" "+ time['$n'];
+  //String jsonData = JSON.encode(word['$n'] +" "+ time['$n']);
+  return jsonData;
+}
+
+Future<shelf.Response> _echoRequest(shelf.Request request) async {
+  //接受post过来的数据
+  String content = await request.readAsString();
+  //把这个post过来的数据有返回给客户端
+  return new shelf.Response.ok(
+      'server susscefullly get the post data from client is: "${content}');
+}
--- web/main.dart
+++ web/main.dart
@@ -2,11 +2,12 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:math';
 import 'dart:convert';
 import 'package:route/client.dart';
 import 'package:logging/logging.dart';
 import 'urls.dart' as urls;
-import 'dart:math';
+
 
 HttpRequest request;
 List<int> t1 = new List(5);
@@ -20,8 +21,8 @@ int longt=3500,num=2500,shortt=1500;
 void main(){
   querySelector("#btnPRLB").onClick.listen(click1);
   querySelector("#btnCLICK")
-       ..onMouseDown.listen(onText1)
-       ..onMouseUp.listen(onText2);
+    ..onMouseDown.listen(onText1)
+    ..onMouseUp.listen(onText2);
 
   new Router()
     ..addHandler(urls.sign, showSign)//注册界面
@@ -45,48 +46,46 @@ void main(){
     ..listen();
 }
 void click1(MouseEvent e){
-     querySelector("#btnCLICK").text="点我测试";
-     String url = 'http://localhost:8080';
-     request = new HttpRequest();
-     request.onReadyStateChange.listen(onData);
-     request.open('GET', url);
-     request.send(" your jsonndata");
-   }
+  querySelector("#btnCLICK").text="点我测试";
+  String url = 'http://localhost:8080';
+  request = new HttpRequest();
+  request.onReadyStateChange.listen(onData);
+  request.open('GET', url);
+  request.send(" your jsonndata");
+}
 
+void onText1(MouseEvent event) {
+  t1[nowclick]=event.timeStamp;
+}
+void onText2(MouseEvent event) {
+  t2[nowclick]=event.timeStamp-t1[nowclick];
+  nowclick++;
+  if(clicknumber==nowclick-1){
+    String value='';
+    for(int i=0;i<=clicknumber;i++)
+    {
+      if(t2[i]<shortt)
+        value=value+'1';
+      else if( num < t2[i] && t2[i]<longt )
+        value=value+'3';
+      else
+        value=value+'2';
+    }
+    if(value==datalist)
+      querySelector("#btnCLICK").text="you win!";
+    else
+      querySelector("#btnCLICK").text="you lose!";
+  }
+}
 
- void onText1(MouseEvent event) {
-     t1[nowclick]=event.timeStamp;
-   }
- void onText2(MouseEvent event) {
-     t2[nowclick]=event.timeStamp-t1[nowclick];
-     nowclick++;
-     if(clicknumber==nowclick-1){
-         String value='';
-         for(int i=0;i<=clicknumber;i++)
-           {
-             if(t2[i]<shortt)
-                 value=value+'1';
-             else if( num < t2[i] && t2[i]<longt )
-             value=value+'3';
-         else
-         value=value+'2';
-     }
-     if(value==datalist)
-       querySelector("#btnCLICK").text="you win!";
-     else
-       querySelector("#btnCLICK").text="you lose!";
-   }
- }
-
-
- void onData(_) {
-     if (request.readyState == HttpRequest.DONE && request.status == 200) {
-         String jsonData=request.responseText;
-         List<String> dataList=jsonData.split(" ");
-         clicknumber= dataList[1].length-1;
-         datalist=dataList[1];
-         querySelector("#QUES").text= dataList[0];
-       }
-   }
+void onData(_) {
+  if (request.readyState == HttpRequest.DONE && request.status == 200) {
+    String jsonData=request.responseText;
+    List<String> dataList=jsonData.split(" ");
+    clicknumber= dataList[1].length-1;
+    datalist=dataList[1];
+    querySelector("#QUES").text= dataList[0];
+  }
+}
 
 
